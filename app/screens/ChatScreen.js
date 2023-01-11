e8b4fbf1e6dbdcaf19fbac7e84b00049aa4688a5
pOrtiz01 <18plog@queensu.ca>
2023-01-11 18:48:08 -0500

    Stack Navigator added

---
+++ app/screens/ChatScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text} from 'react-native';
+
+function ChatScreen(props) {
+    return (
+        <View>
+            <Text> This is the chat screen</Text>
+        </View>
+    );
+}
+
+export default ChatScreen;
